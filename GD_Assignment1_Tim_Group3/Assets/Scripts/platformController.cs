be132e0f6c8dfd3fbeaefe5a448edf681e15cf09
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2021-08-14 23:13:41 +0200

    Development of player controller and Platforms
    Player Controller:
    -Movement of player on x-axis
    -Jumping
    
    Platforms:
    - Five spawn points that spawn one platform at a time at random
    -Platforms are destroyed when  once  they pass the screen.

---
+++ GD_Assignment1_Tim_Group3/Assets/Scripts/platformController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformController : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+    public float speed;
+
+    private void Awake()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        rBody.velocity = new Vector2(0f, speed);
+      
+    }
+}
