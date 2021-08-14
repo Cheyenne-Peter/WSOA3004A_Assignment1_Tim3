4292e14269da4c0934de402ddb2b553ef7306d7a
Alcatrazisme <81303863+EwanAnfield@users.noreply.github.com>
2021-08-14 19:42:30 +0200

    timing solved and day/night added

--- GD_Assignment1_Tim_Group3/Assets/Scripts/DayNightScript.cs
+++ GD_Assignment1_Tim_Group3/Assets/Scripts/DayNightScript.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using UnityEngine.Experimental.Rendering.Universal;
 
 public class DayNightScript : MonoBehaviour
 {
     [SerializeField] private Gradient lightcolor;
-    [SerializeField] private GameObject light;
+    [SerializeField] private new GameObject light;
 
     private int days;
 
@@ -21,6 +22,15 @@ public class DayNightScript : MonoBehaviour
 
     public onDayChanged dayChanged;
 
+    public bool isNight;
+
+    public Text nightOrDay;
+    
+    public void Start()
+    {
+        isNight = false;
+        nightOrDay.text = "Day";
+    }
     private void Update()
     {
         if (time > 120)
@@ -39,7 +49,25 @@ public class DayNightScript : MonoBehaviour
             canChangeDay = true;
 
         time += Time.deltaTime;
-        light.GetComponent<Light2D>().color = lightcolor.Evaluate(time * 0.008f);
+        light.GetComponent<Light2D>().color = lightcolor.Evaluate(time * 0.00825f);
+
+        if ((int)time == 60)
+        {
+            isNight = true;          
+        }
+        if ((int)time == 120)
+        {
+            isNight = false;
+        }
+
+        if (isNight == true)
+        {
+            nightOrDay.text = "Night";
+        }
+        else
+        {
+            nightOrDay.text = "Day";
+        }
 
     }
 
