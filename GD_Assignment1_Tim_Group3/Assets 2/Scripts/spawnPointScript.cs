4b0c765b03a13034f7e35ff6c241d6e8a3a4b0b1
Alcatrazisme <81303863+EwanAnfield@users.noreply.github.com>
2021-08-15 14:24:01 +0200

    works
    YAY - BIGBRAIN

---
+++ GD_Assignment1_Tim_Group3/Assets 2/Scripts/spawnPointScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPointScript : MonoBehaviour
+{
+    public GameObject spawnedObsticle;
+
+    void Start()
+    {
+        Instantiate(spawnedObsticle, transform.position, Quaternion.identity);
+        
+    }
+}
