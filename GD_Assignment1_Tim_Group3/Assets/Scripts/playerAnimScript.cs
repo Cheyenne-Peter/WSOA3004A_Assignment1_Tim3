d8e9e3443dbde06d4f68db31ad192d7c2f46ab29
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2021-08-22 21:36:56 +0200

    Implementation  of Animations
    A new script was added to  the player called "playerAnimScript". Also I kept the old player (the grey box version ) just incase we still need it for some reason . the anim script was added to Player 2

---
+++ GD_Assignment1_Tim_Group3/Assets/Scripts/playerAnimScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimScript : MonoBehaviour
+{
+    private Animator playerAnim; // accesses animator attached to player.
+
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            playerAnim.SetBool("isWalking", true);// Allows the isWalking bool in animator to be called.
+        }
+        else 
+        {
+            playerAnim.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerAnim.SetBool("isJumping", true); // Allows "jump" trigger in  animator to be called.
+
+
+        }
+        else 
+        {
+            playerAnim.SetBool("isJumping", false);
+
+        }
+            
+    }
+}
